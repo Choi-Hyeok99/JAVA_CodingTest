5745f1aa31e3efd49734319030b914ecfd00d3d7
Choi-Hyeok99 <gur0709@naver.com>
2023-07-27 16:04:45 +0900

    알고리즘 공부(대소문자 변환 추가)

--- src/main/java/프로그래머스 코딩/level_0/CaseConversion.java
+++ src/main/java/프로그래머스 코딩/level_0/CaseConversion.java
@@ -1,16 +1,27 @@
+package com.example.start;
+
 import java.util.Scanner;
 
 public class CaseConversion {
     public String solution(String str){
         String answer="";
 
-        for(char x : str.toCharArray()) {
-            if (Character.isLowerCase(x)) {
-                answer += Character.toUpperCase(x);
+        // 1) 답안지
+//        for(char x : str.toCharArray()) {
+//            if (Character.isLowerCase(x)) { // 만약 문자열이 소문자이면
+//                answer += Character.toUpperCase(x); // 대문자로 변환
+//
+//            }else // 그 외 대문자들은
+//                answer+= Character.toLowerCase(x); // 소문자로 변환
+//
+//        }
 
-            }else
-                answer+= Character.toLowerCase(x);
-
+        // 2) 답안지 아스키코드(대소문자 변호)  이용
+        for (char x : str.toCharArray()){
+            if (x>=65 && x<=90){
+                answer+=(char)(x+32);
+            } else
+                answer+=(char)(x-32);
         }
         return answer;
     }
